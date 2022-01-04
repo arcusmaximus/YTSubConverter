39150e35c0b8a41690d5c59603c93aa16bf3ebdb
arc_ <a@a.com>
2022-01-04 16:44:11 +0100

    #55 Ignore color animations for karaoke sections with current word hilighting

--- YTSubConverter.Shared/Formats/Ass/KaraokeTypes/SimpleKaraokeType.cs
+++ YTSubConverter.Shared/Formats/Ass/KaraokeTypes/SimpleKaraokeType.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using YTSubConverter.Shared.Animations;
 
 namespace YTSubConverter.Shared.Formats.Ass.KaraokeTypes
 {
@@ -12,18 +13,25 @@ namespace YTSubConverter.Shared.Formats.Ass.KaraokeTypes
             foreach (AssSection singingSection in context.SingingSections)
             {
                 if (!singingSection.CurrentWordForeColor.IsEmpty)
+                {
                     singingSection.ForeColor = singingSection.CurrentWordForeColor;
+                    singingSection.Animations.RemoveAll(a => a is ForeColorAnimation);
+                }
 
                 if (!singingSection.CurrentWordShadowColor.IsEmpty)
                 {
                     foreach (ShadowType shadowType in singingSection.ShadowColors.Keys.ToList())
                     {
                         singingSection.ShadowColors[shadowType] = singingSection.CurrentWordShadowColor;
+                        singingSection.Animations.RemoveAll(a => a is ShadowColorAnimation shadowAnim && shadowAnim.ShadowType == shadowType);
                     }
                 }
 
                 if (!singingSection.CurrentWordOutlineColor.IsEmpty && singingSection.ShadowColors.ContainsKey(ShadowType.Glow))
+                {
                     singingSection.ShadowColors[ShadowType.Glow] = singingSection.CurrentWordOutlineColor;
+                    singingSection.Animations.RemoveAll(a => a is ShadowColorAnimation shadowAnim && shadowAnim.ShadowType == ShadowType.Glow);
+                }
             }
 
             return new[] { context.StepLine };
