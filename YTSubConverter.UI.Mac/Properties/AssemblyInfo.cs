a6ece81f0d79190e1a738d9eee546b75eae76790
arc_ <a@a.com>
2022-03-10 20:12:35 +0100

    Update version number

--- YTSubConverter.UI.Mac/Properties/AssemblyInfo.cs
+++ YTSubConverter.UI.Mac/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("YTSubConverter")]
 [assembly: AssemblyProduct("YTSubConverter")]
-[assembly: AssemblyVersion("1.6.0")]
-[assembly: AssemblyFileVersion("1.6.0")]
+[assembly: AssemblyVersion("1.6.1")]
+[assembly: AssemblyFileVersion("1.6.1")]
--- YTSubConverter.UI.Win/Properties/AssemblyInfo.cs
+++ YTSubConverter.UI.Win/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("YTSubConverter")]
 [assembly: AssemblyProduct("YTSubConverter")]
-[assembly: AssemblyVersion("1.6.0")]
-[assembly: AssemblyFileVersion("1.6.0")]
+[assembly: AssemblyVersion("1.6.1")]
+[assembly: AssemblyFileVersion("1.6.1")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("fe339cad-14bd-43db-a700-5ddc8b75f43b")]
