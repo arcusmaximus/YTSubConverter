52cbcc8224a4bc0f5cf8e784bdb0e29554274ce8
arc_ <a@a.com>
2022-03-19 16:36:05 +0100

    Update version numbers

--- YTSubConverter.UI.Mac/Properties/AssemblyInfo.cs
+++ YTSubConverter.UI.Mac/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("YTSubConverter")]
 [assembly: AssemblyProduct("YTSubConverter")]
-[assembly: AssemblyVersion("1.6.1")]
-[assembly: AssemblyFileVersion("1.6.1")]
+[assembly: AssemblyVersion("1.6.2")]
+[assembly: AssemblyFileVersion("1.6.2")]
